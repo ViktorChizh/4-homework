2a80445ec8e4dbee3441460518bec98171988da1
ViktorChizh <victor.chizh@mail.ru>
2024-02-06 15:46:24 +0300

    3-sprint 1 homework

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from 'react'
-import {NavLink} from 'react-router-dom'
+import {NavLink, useLocation} from 'react-router-dom'
 import s from './Sidebar.module.css'
 import {PATH} from '../Pages'
 import closeIcon from './closeOutline.svg'
@@ -12,6 +12,8 @@ type PropsType = {
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
+    const location = useLocation()
+    const currentPath = location.pathname
     return (
         <>
             {/*затемнение справа от открытого меню*/}
@@ -32,6 +34,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
                         // className={...} // делает студент
+                        className={currentPath=== '/pre-junior' ? s.active : ''}
                     >
                         Pre-junior
                     </NavLink>
@@ -40,6 +43,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.JUNIOR}
                         onClick={handleClose}
                         // className={...} // делает студент
+                        className={currentPath=== '/junior' ? s.active : ''}
                     >
                         Junior
                     </NavLink>
@@ -48,6 +52,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
                         // className={...} // делает студент
+                        className={currentPath=== '/junior-plus' ? s.active : ''}
                     >
                         Junior Plus
                     </NavLink>
