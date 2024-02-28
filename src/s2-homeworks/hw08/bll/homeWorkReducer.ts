43746c73e5937ec2eb957a7252fde0a03496ccb9
ViktorChizh <victor.chizh@mail.ru>
2024-02-28 17:09:06 +0300

    3-sprint 4 homework

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,18 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+             if(action.payload === 'up'){
+                 return [...state].sort((a: UserType,b: UserType)=>a.name.localeCompare(b.name))
+            } else {
+                 return [...state].sort((a: UserType,b: UserType)=>b.name.localeCompare(a.name))
+            }
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(e=> e.age>=action.payload)
         }
         default:
             return state
