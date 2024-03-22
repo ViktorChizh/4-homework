392c27695306b95f2b833cf277c39f0d97ba4f96
ViktorChizh <victor.chizh@mail.ru>
2024-03-22 13:38:14 +0300

    4-sprint 2 homework

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,11 +1,14 @@
 const initState = {
-    isLoading: false,
+    isLoading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+
+export const loadingReducer = (state = initState, action: LoadingReducerActionType): LoadingReducerStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING': {
+            return {...state, isLoading: action.isLoading}
+        }
         default:
             return state
     }
@@ -15,6 +18,8 @@ type LoadingActionType = {
     type: 'CHANGE_LOADING'
     isLoading: boolean
 }
+export type LoadingReducerStateType = typeof initState
+export type LoadingReducerActionType = ReturnType<typeof loadingAC>
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',
